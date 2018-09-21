9b48f8de49688a1ae06cf3cecacf4eb7dbbf023e
RedKain <RedKain@DESKTOP-AQGCIIQ>
2018-09-21 12:02:02 +0300

    Созданы геттеры в классе Note

--- NoteAppUI/NoteApp/Class1.cs
+++ NoteAppUI/NoteApp/Class1.cs
@@ -9,11 +9,31 @@ namespace NoteApp
 {
     public class Note
     {
-        string Name;
-        enum NoteCategory;
-        string Text;
-        DateTime MakeDate;
-        DateTime LastEditDate;
-
+        private string _name;
+        string Name
+        {
+            get => _name;
+            set
+            {
+                if (value.Length > 50)
+                {
+                    throw new ArgumentException("Длина имени не должна быть более 50 символов, а подается" + value.Length);
+                }
+                else
+                {
+                    _name = value;
+                }
+            }
+        }
+        Category NoteCategory;
+        private string _text;
+        string Text
+        { get => _text; }
+        private DateTime _makeDate;
+        DateTime MakeDate
+        { get => _makeDate;}
+        private DateTime _lastEditDate;
+        DateTime LastEditDate
+        { get => _lastEditDate;}
     }
 }
